aaf1e2f58399b8e00b78b4bd1e8b45c37edef090
play452 <oscarlin@edu.schools.us.kg>
2024-10-02 08:45:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const tmate = 'https://moonlightpanel.xyz/';
+const tmate = 'https://help.moonlightpanel.xyz/';
 
 const proxy = createProxyMiddleware({
   target: tmate,
